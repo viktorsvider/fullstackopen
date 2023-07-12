1e1098f348e64301d14d811f7bcc6e11ccc4ebfe
Viktor Svider <viktor1svider@gmail.com>
2023-07-12 15:41:50 +0300

    add exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    console.log("button clicked", event.target);
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((persona) => (
+        <div key={persona.name}>{persona.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
