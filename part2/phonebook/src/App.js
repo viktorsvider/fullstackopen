2043bc177fe9faf132f2c720e88c0176e3a42ee6
Viktor Svider <viktor1svider@gmail.com>
2023-07-14 10:06:13 +0300

    add exercise 2.13

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,14 @@ import { useState, useEffect } from "react";
 import Phonebook from "./components/Phonebook";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
-import axios from "axios";
+import phonebookService from "./services/phonebook";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
-  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  // ]);
+
   useEffect(() => {
     console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled");
-      setPersons(response.data);
-    });
+    phonebookService.getAll().then((data) => setPersons(data));
   }, []);
 
   const [newName, setNewName] = useState("");
@@ -57,10 +50,9 @@ const App = () => {
     }
 
     if (!isPersonPresent(persons, personObject)) {
-      setPersons(persons.concat(personObject));
-      axios
-        .post("http://localhost:3001/persons", personObject)
-        .catch((error) => console.log(error));
+      phonebookService
+        .create(personObject)
+        .then(setPersons(persons.concat(personObject)));
     } else {
       alert(`${personObject.name} is already added to a phonebook`);
     }
