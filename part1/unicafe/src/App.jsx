f223655869b2593420067f859a6b96f31aed9c23
Viktor Svider <viktor1svider@gmail.com>
2024-09-08 18:31:12 +0300

    add part 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,11 +9,17 @@ const Button = ({ handleClick, text }) => {
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good * 100) / all;
   return (
     <p>
       good {good} <br />
       neutral {neutral} <br />
       bad {bad} <br />
+      all {all} <br />
+      average {Number.isNaN(average) ? 0 : average} <br />
+      positive {Number.isNaN(positive) ? 0 : positive} % <br />
     </p>
   );
 };
