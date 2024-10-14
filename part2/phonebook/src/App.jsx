09785c5f2f52bcaeb7fb505f9a7438732ce4af9f
viktorsvider <viktor1svider@gmail.com>
2024-10-14 21:06:35 +0000

    add 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "+380833332211" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     if (persons.find((element) => element.name === newPerson.name)) {
       alert(`${newPerson.name} already added to the phonebook`);
+    } else if (newName.trim() === "") {
+      alert("Name could not be void");
+    } else if (newNumber.trim() === "") {
+      alert("Number could not be void");
     } else {
       const newPersons = persons.concat(newPerson);
       setPersons(newPersons);
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
-    console.log("change name to", event.target.value);
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -26,7 +37,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name:
+          <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -35,7 +50,10 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <div key={person.name}> {person.name}</div>
+          <div key={person.name}>
+            {" "}
+            {person.name} {person.number}
+          </div>
         ))}
       </div>
     </div>
