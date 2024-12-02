6492ecbedf1a75682d293ea0dfcecbef55850b55
viktorsvider <viktor1svider@gmail.com>
2024-12-02 20:51:18 +0000

    add part 2.20

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -3,20 +3,25 @@ import weather from "../services/weather";
 
 const Weather = ({ city }) => {
   const [cityWeather, setCityWeather] = useState(null);
+  const [iconUrl, setIconUrl] = useState(null);
   useEffect(() => {
     weather
       .getWeatherCity(city)
-      .then((response) => setCityWeather(response.data))
+      .then((response) => {
+        setCityWeather(response.data);
+        setIconUrl(
+          `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+      })
       .catch((error) => console.error(error));
   }, []);
 
-  console.log(" redraw weather=", cityWeather);
   if (cityWeather) {
     return (
       <div>
         <h2>Weather in {city}</h2>
         <p>temperature: {cityWeather.main.temp}</p>
-        <img alt="no icon"></img>
+        <img src={iconUrl} alt="no icon"></img>
         <p>wind: {cityWeather.wind.speed} m/s</p>
       </div>
     );
