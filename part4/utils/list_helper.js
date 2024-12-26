ad8acbfef9c1bb580a569a38b5f95b79824c6b1a
viktorsvider <viktor1svider@gmail.com>
2024-12-26 13:44:39 +0000

    add part 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,12 @@
-const dummy = () => {
+const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const result = blogs.reduce((accamulator, currentValue) => {
+    return accamulator + currentValue.likes;
+  }, 0);
+  return result;
+};
+
+module.exports = { dummy, totalLikes };
