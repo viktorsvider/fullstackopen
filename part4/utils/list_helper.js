9e88abd3c80b46e21d772c9fd7b7191e5280eddc
viktorsvider <viktor1svider@gmail.com>
2024-12-26 19:10:24 +0000

    add part 4.5 no lodash

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -21,4 +21,23 @@ const favouriteBlog = (blogs) => {
   return favourite;
 };
 
-module.exports = { dummy, totalLikes, favouriteBlog };
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  const blogsByAuthor = new Map();
+  blogs.forEach((blog) => {
+    blogsByAuthor.set(blog.author, (blogsByAuthor.get(blog.author) || 0) + 1);
+  });
+
+  let top = { author: "", blogs: 0 };
+  blogsByAuthor.forEach((count, author) => {
+    if (count > top.blogs) {
+      top = { author, blogs: count };
+    }
+  });
+
+  return top;
+};
+
+module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs };
