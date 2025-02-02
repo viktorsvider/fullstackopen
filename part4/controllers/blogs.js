1fd092098c48920cc0e0b8646b51515d35cbc170
Viktor Svider <viktor1svider@gmail.com>
2025-02-02 21:23:36 +0000

    add 4.13 and refactor routers to await

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,23 +1,51 @@
-const blogRouter = require("express").Router();
+const express = require("express");
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response, next) => {
-  Blog.find({}).then((blogs) => {
+const blogRouter = express.Router();
+
+blogRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
     response.json(blogs);
-  }).catch(error => next(error));
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogRouter.post("/", (request, response, next) => {
-  const blogObject = {
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes || 0,
-  };
-  const blog = new Blog(blogObject);
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  }).catch(error => next(error));
+blogRouter.post("/", async (request, response, next) => {
+  try {
+    const { title, author, url, likes } = request.body;
+    if (!title || !url) {
+      return response.status(400).json({ error: "Title and URL are required" });
+    }
+
+    const blog = new Blog({
+      title,
+      author,
+      url,
+      likes: likes || 0,
+    });
+
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  } catch (error) {
+    next(error);
+  }
 });
+
+
+blogRouter.delete("/:id", async (request, response, next) => {
+  try {
+    const deletedNote = await Person.findByIdAndDelete(request.params.id)
+    if (deletedNote) {
+      response.status(204).end()
+    } else {
+      response.status(404).send({ error: 'Document not found' })
+    }
+  }
+  catch {
+    next(error)
+  }
+})
 
 module.exports = blogRouter;
