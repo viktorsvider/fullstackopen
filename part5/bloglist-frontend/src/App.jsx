e37cc467063287a5d9c30d84897a5513b81e9064
Viktor Svider <viktor1svider@gmail.com>
2025-06-04 11:35:12 +0000

    add part 5.7

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -125,7 +125,7 @@ const App = () => {
     <p>{user.name} logged in
       <button onClick={handleLogout}>logout</button>
     </p>
-    <Togglable buttonLabel="new blog" ref={blogFormRef}>
+    <Togglable buttonLabel="create new blog" ref={blogFormRef}>
       <BlogForm
         createBlog={addBlog}
       ></BlogForm>
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react"
+
 const Blog = ({ blog }) => {
-  return <div>"{blog.title}" by {blog.author}</div>
+  const [detailsExposed, setDetailsExposed] = useState(false)
+
+  const handleShowDetails = () => {
+    setDetailsExposed(true)
+  }
+  const handleCollapseDetails = () => {
+    setDetailsExposed(false)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (detailsExposed) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title}
+          <button onClick={handleCollapseDetails}>hide</button>
+        </div>
+        <div>
+          {blog.url ? "no url" : blog.url}
+        </div>
+        <div>
+          {blog.likes ? 0 : blog.likes}
+          <button>like</button>
+        </div>
+        <div>
+          {blog.author}
+        </div>
+      </div>
+    )
+  }
+  else {
+    return <div style={blogStyle}>{blog.title}<button onClick={handleShowDetails}>view</button></div>
+  }
 }
 
 export default Blog
